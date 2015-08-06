20a49450511abe2d7dd3f913967ccf064a1a5d4b
Lit Phansiri <phansiri@uw.edu>
2015-08-06 06:25:09 +0000

    Added everything up to the bs4'ing it in forms.py

--- archive/urls.py
+++ archive/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.url_list, name='url_list'),
     url(r'^url/(?P<pk>[0-9]+)/$', views.url_detail, name='url_detail'),
+    url(r'^url/new/$', views.url_new, name='url_new'),
+    url(r'^url/(?P<pk>[0-9]+)/edit/$', views.url_edit, name='url_edit'),
 ]
--- archive/views.py
+++ archive/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import urlSites
+from .forms import UrlForm
 from bs4 import BeautifulSoup
 import requests
 
@@ -12,3 +13,26 @@ def url_list(request):
 def url_detail(request, pk):
     urlDetail = get_object_or_404(urlSites, pk=pk)
     return render(request, 'archive/url_detail.html', {'urlDetail': urlDetail})
+
+def url_new(request):
+    if request.method == "POST":
+        form = UrlForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.save()
+            return redirect('archive.views.url_detail', pk=post.pk)
+    else:
+        form = UrlForm()
+    return render(request, 'archive/url_edit.html', {'form': form})
+
+def url_edit(request, pk):
+    post = get_object_or_404(urlSites, pk=pk)
+    if request.method == "POST":
+        form = UrlForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.save()
+            return redirect('archive.views.url_detail', pk=post.pk)
+    else:
+        form = UrlForm(instance=post)
+    return render(request, 'archive/url_edit.html', {'form': form})
