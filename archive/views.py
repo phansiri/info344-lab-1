376222452706b9d2eb9908832aa07d0c498d2dad
Lit Phansiri <phansiri@uw.edu>
2015-08-08 05:40:02 +0000

    Finished product

--- archive/views.py
+++ archive/views.py
@@ -12,6 +12,12 @@ def url_list(request):
 
 def url_detail(request, pk):
     urlDetail = get_object_or_404(urlSites, pk=pk)
+    if request.method == "POST":
+        urlDetail.delete()
+        # urlList = urlSites.objects.all().order_by('id')
+        return redirect('archive.views.url_list')
+    else:
+        urlDetail = get_object_or_404(urlSites, pk=pk)
     return render(request, 'archive/url_detail.html', {'urlDetail': urlDetail})
 
 def url_new(request):
@@ -19,6 +25,13 @@ def url_new(request):
         form = UrlForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
+            source_code = requests.request('GET', post.originalUrl)
+            # source_code = requests.get(form)
+            plain_text = source_code.text
+            soup = BeautifulSoup(plain_text, "html.parser")
+            post.finalUrl = source_code.url
+            post.httpStatusCode = source_code.status_code
+            post.pageTitle = soup.title.string
             post.save()
             return redirect('archive.views.url_detail', pk=post.pk)
     else:
