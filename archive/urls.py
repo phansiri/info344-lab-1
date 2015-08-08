389489abd682c7f104898188f7327583442adc46
Lit Phansiri <phansiri@uw.edu>
2015-08-08 06:49:31 +0000

    reorganized delete function along with adding login capabilities

--- archive/urls.py
+++ archive/urls.py
@@ -1,9 +1,20 @@
+# Lab 1
 from django.conf.urls import url
 from . import views
 
+# Lab 2
+from django.contrib import admin
+admin.autodiscover()
+
 urlpatterns = [
+    # Lab 1
     url(r'^$', views.url_list, name='url_list'),
     url(r'^url/(?P<pk>[0-9]+)/$', views.url_detail, name='url_detail'),
     url(r'^url/new/$', views.url_new, name='url_new'),
     url(r'^url/(?P<pk>[0-9]+)/edit/$', views.url_edit, name='url_edit'),
+    url(r'^url/(?P<pk>[0-9]+)/delete/$', views.url_delete, name='url_delete'),
+
+    # Lab 2
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
 ]
--- archive/views.py
+++ archive/views.py
@@ -1,32 +1,33 @@
+# Lab 1 - imports
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import urlSites
 from .forms import UrlForm
 from bs4 import BeautifulSoup
 import requests
 
+# Lab 2 - imports
+from django.contrib.auth.decorators import login_required
 
+
+
+# Lab 1 - url expander
+@login_required
 def url_list(request):
-    # source_code = requests.get(urlSites.objects.get)
     urlList = urlSites.objects.all().order_by('id')
     return render(request, 'archive/url_list.html', {'urlList': urlList})
 
+@login_required
 def url_detail(request, pk):
     urlDetail = get_object_or_404(urlSites, pk=pk)
-    if request.method == "POST":
-        urlDetail.delete()
-        # urlList = urlSites.objects.all().order_by('id')
-        return redirect('archive.views.url_list')
-    else:
-        urlDetail = get_object_or_404(urlSites, pk=pk)
     return render(request, 'archive/url_detail.html', {'urlDetail': urlDetail})
 
+@login_required
 def url_new(request):
     if request.method == "POST":
         form = UrlForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
             source_code = requests.request('GET', post.originalUrl)
-            # source_code = requests.get(form)
             plain_text = source_code.text
             soup = BeautifulSoup(plain_text, "html.parser")
             post.finalUrl = source_code.url
@@ -38,6 +39,7 @@ def url_new(request):
         form = UrlForm()
     return render(request, 'archive/url_edit.html', {'form': form})
 
+@login_required
 def url_edit(request, pk):
     post = get_object_or_404(urlSites, pk=pk)
     if request.method == "POST":
@@ -49,3 +51,12 @@ def url_edit(request, pk):
     else:
         form = UrlForm(instance=post)
     return render(request, 'archive/url_edit.html', {'form': form})
+
+@login_required
+def url_delete(request, pk):
+    urlDelete = get_object_or_404(urlSites, pk=pk)
+    urlDelete.delete()
+    return redirect('archive.views.url_list')
+
+# Lab 2 - Wayback API and Authentication
+
